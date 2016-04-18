a562f627d10c9223004f4863963355b0c5b838f8
Luis Rojas <lrojas94@gmail.com>
2016-04-18 10:48:03 -0400

    Added initial EQUALS operation version based on SJMR algorithm.
    Note that it does not uses indexes yet, but as spatial hadoop has some problems with indexes so far, this seemed like a not-so-bad approach.

--- src/main/java/edu/umn/cs/spatialHadoop/operations/Main.java
+++ src/main/java/edu/umn/cs/spatialHadoop/operations/Main.java
@@ -77,7 +77,10 @@ public class Main {
 
       pgd.addClass("generate", RandomSpatialGenerator.class,
           "Generates a random file containing spatial data");
-
+      
+      pgd.addClass("equals", Equals.class,
+              "Returns the number of shapes equal to each other.");
+      
       pgd.addClass("union", Union.class,
           "Computes the union of input shapes");
 
